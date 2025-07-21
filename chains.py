ed064ce7bdb40ac26f7847bef417e5679c96edec
Maxine <22280915+calmcat2@users.noreply.github.com>
2025-07-20 19:52:54 -0600

    add branch 2

---
+++ chains.py
@@ -0,0 +1,35 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+reflection_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a viral twitter influencer grading a tweet. Generate critique and recommendations for the user's tweet."
+            "Always provide detailed recommendations, including requests for length, virality, style, etc.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+generation_prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a twitter techie influencer assistant tasked with writing excellent twitter posts."
+            " Generate the BEST twitter post possible for the user's request."
+            " If the user provides critique, respond with a one revised version of your previous attempts."
+            "You don't need to give reasons why you revise in a certain way, just show your best version of twitter.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+generate_chain = generation_prompt | llm
+reflect_chain = reflection_prompt | llm
--- main.py
+++ main.py
@@ -1,36 +1,64 @@
-from nodes import run_agent_reasoning, tool_node
+import logging
+from langchain_core.messages import AnyMessage, HumanMessage, AIMessage
+from langgraph.graph import MessageGraph, END
+from chains import generate_chain, reflect_chain
 from dotenv import load_dotenv
-from langchain_core.messages import HumanMessage
-from langgraph.graph import MessagesState, StateGraph, END
+from typing import List
 
 load_dotenv()
 
-AGENT_REASON = "agent_reason"
-ACT = "act"
-LAST = -1
 
-def should_continue(state: MessagesState) -> str:
-    if not state["messages"][LAST].tool_calls:
+GENERATE = "generate"
+REFLECT = "reflect"
+
+
+def generation_node(state: List[AnyMessage]) -> AnyMessage:
+    print("------------------GENERATE node called------------------")
+    response = generate_chain.invoke({"messages": state})
+    # print(response.content)
+    return [AIMessage(content=response.content)]
+
+
+def reflection_node(state: List[AnyMessage]) -> List[HumanMessage]:
+    print("------------------REFLECT node called------------------")
+    response = reflect_chain.invoke({"messages": state})
+    # print(response.content)
+    return [HumanMessage(content=response.content)]
+
+
+def should_continue(state: List[AnyMessage]):
+    if len(state) > 4:
         return END
-    return ACT
+    return REFLECT
 
-flow = StateGraph(MessagesState)
 
-flow.add_node(AGENT_REASON, run_agent_reasoning)
-flow.set_entry_point(AGENT_REASON)
-flow.add_node(ACT, tool_node)
+def create_graph():
+    flow = MessageGraph()
+    flow.add_node(GENERATE, generation_node)
+    flow.add_node(REFLECT, reflection_node)
+    flow.set_entry_point(GENERATE)
+    flow.add_conditional_edges(
+        GENERATE, should_continue, {END: END, REFLECT: REFLECT}
+    )
+    flow.add_edge(REFLECT, GENERATE)
+    app = flow.compile()
+    app.get_graph().draw_mermaid_png(output_file_path="graph.png")
+    return app
 
-flow.add_conditional_edges(AGENT_REASON, should_continue, {
-    END: END,
-    ACT: ACT})
 
-flow.add_edge(ACT, AGENT_REASON)
+def main():
+    app = create_graph()
+    print("start reflection agent.")
+    query = (
+        "Make this tweet better: '@LangChainAI — newly Tool Calling feature is "
+        "seriously underrated. After a long wait, it's here- making the "
+        "implementation of agents across different models with function "
+        "calling - super easy. Made a video covering their newest blog post'"
+    )
+    final_response = app.invoke(HumanMessage(content=query))
+    print("------------------FINAL RESPONSE------------------")
+    print(final_response[-1].content)
 
-app = flow.compile()
-app.get_graph().draw_mermaid_png(output_file_path="graph.png")
 
 if __name__ == "__main__":
-    print("Running ReAct LangGraph with Function Calling...")
-    query = "What's the Temperature in SLC Utah? Double the result."
-    res = app.invoke({"messages": [HumanMessage(content=query)]})
-    print(res["messages"][LAST].content)
+    main()
